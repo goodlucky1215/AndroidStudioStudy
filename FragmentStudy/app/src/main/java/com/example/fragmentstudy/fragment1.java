f608046ec2bb9e93cde424956f65667d5f901823
goodlucky1215 <goodlucky1215@naver.com>
2021-07-01 02:55:21 +0900

    프래그먼트 복습

---
+++ FragmentStudy/app/src/main/java/com/example/fragmentstudy/fragment1.java
@@ -0,0 +1,28 @@
+package com.example.fragmentstudy;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class fragment1 extends Fragment {
+
+    Button btn_start, btn_stop;
+
+    public fragment1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+}
