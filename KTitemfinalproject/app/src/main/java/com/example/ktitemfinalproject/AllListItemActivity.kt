0107ab8005361b8eb874bdac1eab12fc9cdbb865
goodlucky1215 <goodlucky1215@naver.com>
2021-07-11 03:00:20 +0900

    내동생-아이템 페이지부분

---
+++ KTitemfinalproject/app/src/main/java/com/example/ktitemfinalproject/AllListItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ktitemfinalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllListItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_list_item)
+    }
+}
