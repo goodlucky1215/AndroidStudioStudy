9fb32a18f6f0503ff5dea8927a474e1cb2788b77
goodlucky1215 <goodlucky1215@naver.com>
2021-08-09 20:41:46 +0900

    book802

--- KTBook802/app/src/main/java/com/example/ktbook802/MainActivity.kt
+++ KTBook802/app/src/main/java/com/example/ktbook802/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
         setContentView(binding.root)
         adapter = BookAdapter()
         binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
